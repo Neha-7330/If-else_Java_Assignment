7e02563590260863d5f1220ad4c8e67002e170ab
Neha Pudake <126329167+Neha-7330@users.noreply.github.com>
2023-09-26 00:43:22 +0530

    Add files via upload

---
+++ Assignment_01.java
@@ -0,0 +1,227 @@
+
+class assign1{
+    /*
+    1] Program check if the number is even or odd 
+     
+    public static void main(String[] args){
+        int x = 0;
+        if (x % 2 == 0){
+            System.out.println(x + " is an even number.");
+        }
+        else{
+            System.out.println(x + " is an odd number.");
+        }
+    }
+    */
+
+
+    /*
+    2] take a number and print whether it is less than 10 or greater than 10.
+    
+
+    public static void main(String[] args){
+        int var = 11;
+        if(var > 10){
+            System.out.println(var + " is greater than 10.");
+        }
+        else if (var == 10){
+            System.out.println(var + " is equal to 10.");
+        }
+        else{
+            System.out.println(var + " is less than 10.");
+        }
+    }
+    */
+
+
+    /*
+    3] take a number, and print whether it is positive or negative 
+    
+    public static void main(String[] args){
+        int x = -2;
+        if(x >= 0){
+            System.out.println(x + " is a positive number.");
+        }
+        else{
+            System.out.println(x + " is a negative number.");
+        }
+    }
+    */
+
+
+    /* 
+    4] Program that checks a number from 0 to 5 and print its spelling. if the number is 
+       greater than 5 print the number is greater than 5.
+    
+    public static void main(String[] args){
+        int num = 6;
+        if(num == 0){
+            System.out.println(num + " : zero");
+        }
+        else if(num == 1){
+            System.out.println(num + " : one");
+        }
+        else if(num == 2){
+            System.out.println(num + " : two");
+        }
+        else if(num == 3){
+            System.out.println(num + " : three");
+        }
+        else if(num == 4){
+            System.out.println(num + " : four");
+        }
+        else if(num == 5){
+            System.out.println(num + " : five");
+        }
+        else{
+            System.out.println(num + " is greater than 5.");
+        }
+    }
+    */
+
+
+    /* 
+    5] Program in which according to month no print the no. of days in that month
+    
+    public static void main(String[] args){
+        int num = 0;
+
+        if(num == 1){
+            System.out.println("January has 31 days.");
+        }
+        else if(num == 2){
+            System.out.println("February has 28 days in common year and 29 days in leap years.");
+        }
+        else if(num == 3){
+            System.out.println("March has 31 days.");
+        }
+        else if(num == 4){
+            System.out.println("April has 30 days.");
+        }
+        else if(num == 5){
+            System.out.println("May has 31 days.");
+        }
+        else if(num == 6){
+            System.out.println("June has 30 days.");
+        }
+        else if(num == 7){
+            System.out.println("July has 31 days.");
+        }
+        else if(num == 8){
+            System.out.println("August has 31 days.");
+        }
+        else if(num == 9){
+            System.out.println("September has 30 days.");
+        }
+        else if(num == 10){
+            System.out.println("October has 31 days.");
+        }
+        else if(num == 11){
+            System.out.println("November has 30 days.");
+        }
+        else if(num == 12){
+            System.out.println("December has 31 days.");
+        }
+        else{
+            System.out.println("Invalid month.");
+        }
+    }
+    */
+
+
+    /* 
+    6] Write a program to find maximum between three number.
+    
+    public static void main(String[] args){
+        int num1 = 42;
+        int num2 = 32;
+        int num3 = 42;
+        if (num1 >= num2 && num1 >= num3){
+            System.out.println(num1 + " is the maximum between " + num1 + ", " + num2 + " and " + num3);
+        } 
+        else if (num2 >= num3){
+            System.out.println(num2 + " is the maximum between " + num1 + ", " + num2 + " and " + num3);
+        }
+        else{
+            System.out.println(num3 + " is the maximum between " + num1 + ", " + num2 + " and " + num3);
+        }
+    }
+    */
+
+
+    /* 
+    7] Calculate profit or loss.
+       Write a program that takes the cost price and the selling price and calculate pofit or loss.
+    
+
+    public static void main(String[] args){
+        int sp = 900;
+        int cp = 900;
+
+        if (sp == cp){
+            System.out.println("Nither profit Nor loss.");
+        }
+        else if(sp > cp){
+            System.out.println("Profit of " + (sp - cp));
+        }
+        else{
+            System.out.println("Loss of " + (cp - sp));
+        }
+    }
+    */
+
+
+    /* 
+    8] Write a program to check day number (1-7) and print the corresponding day of week
+    
+    public static void main(String[] args){
+        int num = 8;
+        if(num == 1){
+            System.out.println("Monday");
+        }
+        else if(num == 2){
+            System.out.println("Tuesday");
+        }
+        else if(num == 3){
+            System.out.println("Wednesday");
+        }
+        else if(num == 4){
+            System.out.println("Thursday");
+        }
+        else if(num == 5){
+            System.out.println("Friday");
+        }
+        else if(num == 6){
+            System.out.println("Saturday");
+        }
+        else if(num == 7){
+            System.out.println("Sunday");
+        }
+        else{
+            System.out.println("Invalid input");
+        }
+    }
+    */
+
+
+    /* 
+    9] Write a program in java to accept three number and check whether they are Pythagorean triplets or not.
+    
+    public static void main(String[] args){
+        int a = 2;
+        int b = 2;
+        int c = 2;
+        if( a*a + b*b == c*c){
+            System.out.println("Its Pythagorean triplet.");
+        }
+        else{
+            System.out.println("It is not a Pythagorean triplet.");
+        }
+    }
+    */
+
+
+    /* 
+    10] Real time example :- thicket counter.
+    */
+}
